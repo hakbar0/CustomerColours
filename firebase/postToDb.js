e69be7bfe426429e1f785aa64db6fbcdad64590d
Haseeb Akbar <haseeb2000@hotmail.com>
2018-07-17 22:15:23 +0100

    tidy

--- firebase/postToDb.js
+++ firebase/postToDb.js
@@ -1,6 +1,6 @@
 const db = require("./connect");
 
-function userTest(firstName, surname, telephone, DOB) {
+function postToDb(firstName, surname, telephone, DOB) {
   db.ref("/customers").push({
     firstName,
     surname,
@@ -9,6 +9,4 @@ function userTest(firstName, surname, telephone, DOB) {
   });
 };
 
-userTest("haseeb", "akbar", "0161", "20/02/94");
-
-module.exports = { userTest };
+module.exports = { ostToDb };
