bc88d6c7decade54a75c4575e50ef3c0edb01b7b
Haseeb Akbar <haseeb2000@hotmail.com>
2018-07-21 22:02:32 +0100

    Able to change colors

--- customercolours/src/components/Customers.js
+++ customercolours/src/components/Customers.js
@@ -13,7 +13,8 @@ class Customers extends React.Component {
     dob: '',
     number: '',
     style: 'none',
-    update: false
+    update: false,
+    background: 'red'
   };
 
   getAllUsers = () => { db.ref("/customers").on("value", res => { this.setState({ customers: res.val() }); }) };
@@ -47,8 +48,7 @@ class Customers extends React.Component {
               this.state.customers &&
               Object.entries(this.state.customers).reverse().map((customer) => {
                 return (
-
-                  <tr className="customer" key={customer[0]}
+                  <tr className="customer" key={customer[0]} style={{ background: this.colorChange(customer[1].firstname) }}
                     onClick={this.currentId.bind(this, customer[0], customer[1].firstname,
                       customer[1].surname, customer[1].DOB,
                       customer[1].telephone)}>
@@ -67,7 +67,7 @@ class Customers extends React.Component {
 
         <div id="myModal" className={`modal`} style={{ display: this.state.style }}>
           <div className={`modal-content`}>
-            <span className={`close`} onClick={this.hide.bind(null, "none")}>&times;</span>
+            <span className={`close`} onClick={this.hide.bind(null, "none")} >&times;</span>
             {!this.state.update ?
             <div className ="content">
             <h1>Name: {this.state.firstname}</h1>
@@ -104,6 +104,14 @@ class Customers extends React.Component {
 
       </div>
     )
+  }
+
+  colorChange= (name) => {
+    let nameCount = 0;
+    for(let i =0; i < name.length; i++){nameCount += name.charCodeAt(i);}
+    if(nameCount > 255) nameCount = nameCount%146;
+    let css= ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque",,"BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
+    return css[nameCount];
   }
 
   deleteUser = () => {
