f22271637c84a3984c9c641ad78bd35b8ace06eb
Haseeb Akbar <haseeb2000@hotmail.com>
2018-07-21 16:52:35 +0100

    Able to retrieve data from database and display.

--- customercolours/src/components/Home.js
+++ customercolours/src/components/Home.js
@@ -1,10 +1,50 @@
 import React from 'react';
 import '../styles/Home.css';
+import db from '../firebase/connect';
 
-class Home extends React.Component{
-  render(){
-    return(
-      <div>HOMEPAGE</div>
+class Home extends React.Component {
+
+  state = {
+    customers: ''
+  };
+
+  getAllUsers = () => {
+    db.ref("/customers").on("value", res => {
+      this.setState({ customers: res.val() });
+    })
+  };
+
+  componentDidMount() {
+    this.getAllUsers();
+  }
+
+  render() {
+    return (
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Surname</th>
+              <th>DOB</th>
+              <th>Number</th>
+            </tr>
+            {
+              this.state.customers &&
+              Object.entries(this.state.customers).map(function (customer) {
+                return (
+                  <tr>
+                    <td>{customer[1].firstname}</td>
+                    <td>{customer[1].surname}</td>
+                    <td>{customer[1].DOB}</td>
+                    <td>{customer[1].telephone}</td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+      </div>
     )
   }
 };
--- customercolours/src/firebase/postUsers.js
+++ customercolours/src/firebase/postUsers.js
@@ -2,7 +2,7 @@ import faker from 'faker';
 import { postUser } from './postToDb';
 
 export const postRandomUsers = () => {
-  for (let i = 0; i < 500; i++) {
+  for (let i = 0; i < 50; i++) {
     postUser(
       `${faker.random.number({ min: 1, max: 28 })}/${faker.random.number({ min: 1, max: 12 })}/${faker.random.number({ min: 1920, max: 2000 })}`,
       faker.fake("{{name.firstName}}"),
